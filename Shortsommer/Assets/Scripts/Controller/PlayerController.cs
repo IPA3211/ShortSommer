e721e050fc3c8e91c63780040bc833371822d6a1
ipa3211 <kygsm2003@naver.com>
2024-05-23 18:16:36 +0900

    PlayerController 가 Move이외에 다른 명령도 처리

--- Shortsommer/Assets/Scripts/Controller/PlayerController.cs
+++ Shortsommer/Assets/Scripts/Controller/PlayerController.cs
@@ -1,42 +1,88 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.Rendering.DebugUI;
 
-public class PlayerController : MonoBehaviour, IController
+public class PlayerController : Controller, InputSystem.IPlayerActions
 {
-    public HumanCharacter defaultAttach;
     InputSystem playerInput;
-    ICharacter character;
+    InputStruct input;
 
-    public ICharacter Character => character;
+    public override InputStruct InputInfo => input;
 
-    public void AttachCharacter(ICharacter character)
+    public void OnAimming(InputAction.CallbackContext context)
     {
-        if(this.character == character) return;
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-        DetachCharacter();
-        this.character = character;
-        character.AttachController(this);
-    }
-
-    public void DetachCharacter()
-    {
-        character?.DetachController();
-        character = null;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (playerInput.Player.Move.IsInProgress())
+        if(Physics.Raycast(ray, out hit))
         {
-            character.Move(playerInput.Player.Move.ReadValue<Vector2>());
+            Debug.Log(hit.point);
+        }
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            input.m_fire = true;
+        }
+
+        if (context.canceled)
+        {
+            input.m_fire = false;
+        }
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            input.m_Interact = true;
+        }
+
+        if (context.canceled)
+        {
+            input.m_Interact = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            input.m_Jump = true;
+        }
+
+        if (context.canceled)
+        {
+            input.m_Jump = false;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        input.m_MoveDir = context.ReadValue<Vector2>();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            input.m_Sprint = true;
+        }
+
+        if (context.canceled)
+        {
+            input.m_Sprint = false;
         }
     }
 
     void Start()
     {
         playerInput = new InputSystem();
-        playerInput.Player.Move.Enable();
+        playerInput.Player.Enable();
+
+        playerInput.Player.AddCallbacks(this);
     }
 }
