c64e1f69bac63cb2f286664127b667bc03eea431
ipa3211 <kygsm2003@naver.com>
2024-05-29 02:15:40 +0900

    Input관련 asmdef 나눔

--- Shortsommer/Assets/Scripts/System/TestGameSystem.cs
+++ Shortsommer/Assets/Scripts/System/TestGameSystem.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class TestGameSystem : MonoBehaviour
 {
-    [SerializeField] HumanCharacter defaultCharacter;
+    [SerializeField] SommerCharacter defaultCharacter;
     [SerializeField] PcController defaultController;
     // Start is called before the first frame update
     void Start()
