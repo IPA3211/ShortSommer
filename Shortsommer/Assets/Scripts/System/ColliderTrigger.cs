f64d73465d3264ae46bc2cae9cad496cc5449c47
ipa3211 <kygsm2003@naver.com>
2024-05-29 02:18:03 +0900

    ColliderTrigger 추가

---
+++ Shortsommer/Assets/Scripts/System/ColliderTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColliderTrigger : MonoBehaviour
+{
+    [SerializeField] UnityEvent<Collider> onTriggerEnter;
+    [SerializeField] UnityEvent<Collider> onTriggerStay;
+    [SerializeField] UnityEvent<Collider> onTriggerExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onTriggerEnter?.Invoke(other);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        onTriggerStay?.Invoke(other);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        onTriggerExit?.Invoke(other);
+    }
+}
